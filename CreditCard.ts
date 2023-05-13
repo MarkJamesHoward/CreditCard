03d4f601d2a817378f2e98d9f06650c6d3a6fe2b
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2023-05-13 14:21:25 +1200

    container queries

--- CreditCard.ts
+++ CreditCard.ts
@@ -1,6 +1,6 @@
 import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { choose } from "lit/directives/choose.js";
+import { when } from "lit/directives/when.js";
 
 export const tagName = "credit-card";
 
@@ -15,55 +15,110 @@ export class CreditCard extends LitElement {
   @property()
   plan = "Plan";
 
-  @property()
+  @property({ reflect: true, type: String })
   rate = 0;
 
-  @property()
+  @property({ reflect: true, type: String })
   fee = 0;
+
+  @property({ reflect: true, type: Boolean })
+  airpoints = false;
+
+  @property({ reflect: true, type: String })
+  link;
 
   @property()
   cardImage = {};
 
+  @property({ reflect: true, type: Boolean })
+  fullScreen;
+
   render() {
-    return html`
-      <div
-        style="display: flex; flex-direction: row; justify-content: space-between; 
+    const viewTransCardImage =
+      this.issuer.replaceAll(" ", "") +
+      this.plan.replaceAll(" ", "") +
+      "CardImageVT";
+
+    const viewTransIssuerPlan =
+      this.issuer.replaceAll(" ", "") +
+      this.plan.replaceAll(" ", "") +
+      "IssuerPlanVT";
+
+    return html` ${when(
+      !this.fullScreen,
+      () => html`
+        <link rel="stylesheet" href="./tailwindGenerated.css" />
+        <div
+          class="@container"
+          style="display: flex; flex-direction: row; justify-content: space-between; 
         border-style: solid; border-thickness: 1px; border-color: LightGray;margin: 5px;
         border-radius: 5px; padding: 5px"
-      >
-        <div style="display: flex; flex-direction: row; font-weight: 800">
-          <img style="width:50px;margin-right:5px" src=${this.cardImage} />
-          <div style="display: flex; flex-direction: row">
-            <div style="margin-right: 5px">${this.issuer}</div>
-            <div>${this.plan}</div>
-          </div>
-        </div>
-        <div
-          style="display: flex; flex-direction: column;flex-basis:50%; font-weight: 700"
         >
-          ${choose(
-            this.sortingby,
-            [
-              [
-                "rate",
-                () =>
-                  html`<div>
-                      Purchase Rate
-                      <span style="color: red">${this.rate}%</span>
-                    </div>
-                    <div>Fee $${this.fee}</div>`,
-              ],
-              [
-                "fee",
-                () => html`<div>Purchase Rate ${this.rate}%</div>
-                  <div>Fee <span style="color: red">${this.fee}%</span></div>`,
-              ],
-            ],
-            () => html`<div>Purchase Rate ${this.rate}%</div>
-              <div>Fee ${this.fee}%</div>`
-          )}
+          <div class="flex flex-col @4xl:flex-row font-bold basis-1/3">
+            <img
+              width="100px"
+              style="object-fit: contain;margin-right:5px;view-transition-name: ${viewTransCardImage}"
+              src=${this.cardImage}
+            />
+            <div
+              class="flex flex-col @4xl:flex-row order-first @4xl:order-last"
+              style="view-transition-name: ${viewTransIssuerPlan}"
+            >
+              <div class="mr-2">${this.issuer}</div>
+              <div>${this.plan}</div>
+            </div>
+          </div>
+          <div
+            style="display: flex; flex-direction: column;flex-basis:50%; font-weight: 700"
+          >
+            <div>
+              Purchase Rate
+              ${when(
+                this.sortingby == "rate",
+                () => html`<span style="color: red"> ${this.rate}% </span>`,
+                () => html`<span> ${this.rate} % </span>`
+              )}
+            </div>
+            <div>
+              Fee
+              ${when(
+                this.sortingby == "fee",
+                () => html`<span style="color: red"> $${this.fee}</span>`,
+                () => html`<span> $${this.fee}</span>`
+              )}
+            </div>
+            ${when(
+              this.airpoints,
+              () => html`<div>Airpoints</div>`,
+              () => html``
+            )}
+          </div>
+          <a href=${this.link}>More Details</a>
         </div>
-      </div>
-    `;
+      `,
+      () => html`
+        <link rel="stylesheet" href="./tailwindGenerated.css" />
+        <div class="flex flex-col justify-between items-center h-screen">
+          <div
+            style="view-transition-name:  ${viewTransIssuerPlan}"
+            class="text-5xl font-bold text-center"
+          >
+            ${this.issuer} ${this.plan}
+          </div>
+          <img
+            style="view-transition-name:  ${viewTransCardImage}"
+            class="w-[50vmin]"
+            src="${this.cardImage}"
+          />
+          <!-- <article set:html={content} /> -->
+          <a
+            class="justify-self-end px-6 m-10 text-center py-3 text-blue-100 no-underline
+       bg-blue-500 rounded hover:bg-blue-600 hover:underline hover:text-blue-200"
+            href="/"
+            >Back</a
+          >
+        </div>
+      `
+    )}`;
   }
 }
