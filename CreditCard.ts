5f9146195905ee4bcb8af7cff9e458f6a3ca00f5
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2023-05-20 09:17:12 +1200

    formatting

--- CreditCard.ts
+++ CreditCard.ts
@@ -49,27 +49,29 @@ export class CreditCard extends LitElement {
       () => html`
         <link rel="stylesheet" href="./tailwindGenerated.css" />
         <div
-          class="@container"
-          style="display: flex; flex-direction: row; justify-content: space-between; 
-        border-style: solid; border-thickness: 1px; border-color: LightGray;margin: 5px;
+          class="@container flex flex-row justify-between"
+          style="   border-style: solid; border-thickness: 1px; border-color: LightGray;margin: 5px;
         border-radius: 5px; padding: 5px"
         >
-          <div class="flex flex-col @4xl:flex-row font-bold basis-1/3">
+          <div
+            class="flex flex-col @4xl:items-center @4xl:flex-row font-bold basis-1/3"
+          >
             <img
               width="100px"
+              class="justify-center justify-items-center self-center"
               style="object-fit: contain;margin-right:5px;view-transition-name: ${viewTransCardImage}"
               src=${this.cardImage}
             />
             <div
-              class="flex flex-col @4xl:flex-row order-first @4xl:order-last"
+              class="flex flex-col @4xl:items-center @4xl:flex-row order-first @4xl:order-last"
               style="view-transition-name: ${viewTransIssuerPlan}"
             >
-              <div class="mr-2">${this.issuer}</div>
-              <div>${this.plan}</div>
+              <div class="mr-2 self-center">${this.issuer}</div>
+              <div class="self-center">${this.plan}</div>
             </div>
           </div>
           <div
-            style="display: flex; flex-direction: column;flex-basis:50%; font-weight: 700"
+            class="flex flex-col items-center justify-center justify-items-center font-bold basis-1/2"
           >
             <div>
               Purchase Rate
@@ -93,7 +95,12 @@ export class CreditCard extends LitElement {
               () => html``
             )}
           </div>
-          <a href=${this.link}>More Details</a>
+          <a
+            class="px-6 m-5 inline-block text-center py-3 text-blue-100 no-underline
+       bg-blue-500 rounded hover:bg-blue-600 hover:underline hover:text-blue-200"
+            href=${this.link}
+            >More Details</a
+          >
         </div>
       `,
       () => html`
@@ -111,6 +118,16 @@ export class CreditCard extends LitElement {
             src="${this.cardImage}"
           />
           <!-- <article set:html={content} /> -->
+          <div class="flex flex-row justify-between grow">
+            <div class="text-5xl flex flex-col items-center">
+              <div>Purchase Rate</div>
+              <div>${this.rate}%</div>
+            </div>
+            <div class="text-5xl flex flex-col items-center">
+              <div>Annual Fee</div>
+              <div>$${this.fee}</div>
+            </div>
+          </div>
           <a
             class="justify-self-end px-6 m-10 text-center py-3 text-blue-100 no-underline
        bg-blue-500 rounded hover:bg-blue-600 hover:underline hover:text-blue-200"
